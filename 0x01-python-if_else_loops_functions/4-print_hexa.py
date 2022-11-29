163c1aaaf8a99f8222a3a7358ae8200705f3281b
efodthe2nd <davidefod@gmail.com>
2022-11-29 16:46:23 +0000

    4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(99):
     if i < 10:
-        print("{:d} = 0x{:d}".format(i, i))
+        print("{} = 0x{}".format(i, i))
     elif i > 9:
-        print("{:d} = 0x{:x}".format(i, i))
+        print("{} = 0x{:x}".format(i, i))
