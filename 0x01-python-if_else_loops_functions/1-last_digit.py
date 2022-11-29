2bd9bbc28317653d0e27d4dc3b6a677f75303f31
efodthe2nd <davidefod@gmail.com>
2022-11-29 15:22:24 +0000

    fix spacing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,7 @@ else:
     lastdigit = number % 10
 
 if lastdigit < 6 and lastdigit != 0:
-        print(f"Last digit of {number} is {lastdigit} \
-                and is less than 6 and not 0")
+        print(f"Last digit of {number} is {lastdigit} and is less than 6 and not 0")
 if (lastdigit) > 5:
     print(f"Last digit of {number} is {lastdigit} and is greater than 5")
 elif (number % 10) == 0:
