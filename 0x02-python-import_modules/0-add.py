238e3bb4b92882203a581378421991a108ec58ec
efodthe2nd <davidefod@gmail.com>
2022-12-02 07:40:52 +0000

    fix code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, a + b))
-
-
-if __name__ == "__main__":
-    import sys
-    add(int(sys.argv[1]), int(sys.argv[2]))
+    print("{} + {} = {}".format(a, b, add(a, b)))
