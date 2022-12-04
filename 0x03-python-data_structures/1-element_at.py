d0b1bd302e80293ff7a3fcda40367d6ee2444647
efodthe2nd <davidefod@gmail.com>
2022-12-04 23:36:29 +0000

    Securing access

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return None
+        return ("None")
     elif idx > len(my_list) - 1:
-        return None
+        return ("None")
+
     for i in my_list:
         if i == idx:
             return my_list[i]
+        
