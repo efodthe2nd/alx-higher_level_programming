e195cc96b30d5c72aeab317276460014d998f2e8
efodthe2nd <davidefod@gmail.com>
2022-12-05 08:35:56 +0000

    2s

--- 0x03-python-data_structures/0-main.py
+++ 0x03-python-data_structures/0-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 print_list_integer = __import__('0-print_list_integer').print_list_integer
-
+replace_in_list = __import__('2-replace_in_list').replace_in_list
 my_list = [1,2,4,5]
 print_list_integer(my_list)
+new_list = replace_in_list(my_list, 2, 5)
+print(new_list)
