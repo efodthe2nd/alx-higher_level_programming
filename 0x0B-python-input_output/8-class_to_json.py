51a3227f8f66163f10d0a4ad7dde1e8595ca87c2
efodthe2nd <davidefod@gmail.com>
2023-01-11 13:14:11 +0000

    8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,6 +2,6 @@
 """This module defines a Python class-to-JSON function"""
 
 
-def class_to_jsonobj):
+def class_to_json(obj):
     """Returns the dictionary representation of a simple data structure"""
     return obj.__dict__
