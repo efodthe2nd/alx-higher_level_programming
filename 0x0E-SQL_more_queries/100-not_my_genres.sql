1768571b31cd1222c089b88b99ae7ff20c3624b0
efodthe2nd <davidefod@gmail.com>
2023-02-19 17:50:25 +0000

    100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,10 @@
 -- lists all genres of the show Dexter
 
 SELECT tv_genres.name FROM tv_genres
-WHERE tv_genres.id NOT IN (
-	SELECT tv_genres.id FROM tv_genres
+	WHERE tv_genres.id NOT IN (
+SELECT tv_genres.id FROM tv_genres
 	INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genre_id
 	INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 	WHERE tv_shows.title = 'Dexter')
-GROUP BY tv_genres.name
-ORDER BY tv_genres.name;
+	GROUP BY tv_genres.name
+	ORDER BY tv_genres.name;
