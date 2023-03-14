bd84d144f3e901249f7a1b2627a361ba2d7ea76a
efodthe2nd <davidefod@gmail.com>
2023-03-14 08:49:08 +0000

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (!process.argv[2]) {
+const arg = process.argv;
+if (!arg[2]) {
   console.log("No argument");
 } else {
-  console.log(process.argv[2]);
+  console.log(arg[2]);
 }
