1c42cf9f6b65bbf9de7abd30321bbc9692f02e04
efodthe2nd <davidefod@gmail.com>
2023-03-14 10:17:25 +0000

    7s

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ const len = process.argv.length;
 if (len === 2 || isNaN(args[2])) {
   console.log('Missing number of occurences');
 } else {
-  const num = args[2];
+  const num = +args[2];
   for (let i = 0; i < num; i++) {
   	console.log('C is fun');
   }
