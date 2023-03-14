d209fdc8b644ae6ec15f4fdcf550fdf64eed45cc
efodthe2nd <davidefod@gmail.com>
2023-03-14 09:58:58 +0000

    6s

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 const len = list.length;
 
 for (let i = 0; i < len; i++) {
