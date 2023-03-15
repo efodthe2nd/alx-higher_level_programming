87079aaea1ef10b7da7a3b7843b7771576ce47c1
efodthe2nd <davidefod@gmail.com>
2023-03-15 13:09:45 +0000

    102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,6 +6,6 @@ const secondFile = process.argv[3];
 const targetFile = process.argv[4];
 
 if (fs.existsSync(firstFile) && fs.existsSync(secondFile)) {
-  fs.writeFileSync(targetFile, fs.readFileSync(firstFile)));
+  fs.writeFileSync(targetFile, fs.readFileSync(firstFile));
   fs.appendFileSync(targetFile, fs.readFileSync(secondFile));
 }
