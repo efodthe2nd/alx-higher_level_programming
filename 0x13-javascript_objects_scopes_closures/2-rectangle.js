b5333f720476aa15a5c41469516bed1ac0dd35d8
efodthe2nd <davidefod@gmail.com>
2023-03-15 11:45:51 +0000

    2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w < 1 || h < 1) {
+      return Rectangle;
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+};
