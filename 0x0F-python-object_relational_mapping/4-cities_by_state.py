ebbb05a9a9cdd37698814e6b045a3aee322283db
efodthe2nd <davidefod@gmail.com>
2023-03-19 22:36:00 +0000

    4s

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
                  FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
-                   ON     c`.`state_id` = `s`.`id` \
+                   ON c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
     [print(city) for city in c.fetchall()]
