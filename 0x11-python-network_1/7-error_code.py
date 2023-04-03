b649536670ca86c9cd88de1a9c29a78801e9a28f
David Efod <davidefod@gmail.com>
2023-04-03 21:18:25 +0000

    7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+sends a request to passed URL and displays the body of the response
+based on status_code
+"""
+
+import requests
+import sys
+
+
+def main():
+    url = sys.argv[1]
+
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print('Error code: {}'.format(res.status_code))
+    else:
+        print(res.text)
+
+
+if __name__ == '__main__':
+    main()
