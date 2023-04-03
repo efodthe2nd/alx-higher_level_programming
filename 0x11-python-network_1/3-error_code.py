9a769f5b40e610ae99668def549c55a2f1eb8334
David Efod <davidefod@gmail.com>
2023-04-03 20:31:58 +0000

    3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Sends a POST request to passed URL with email as parameter and displays the body of the respons. Handles urllib.error.HTTPError
+"""
+
+import urllib.request
+import sys
+
+
+def main():
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            byte = response.read()
+        print(byte.decode())
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
+
+
+if __name__ == '__main__':
+    main()
