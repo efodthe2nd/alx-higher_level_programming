1ee94692412ba928dad27065c2add1cc13508c85
David Efod <davidefod@gmail.com>
2023-04-03 20:26:09 +0000

    2s

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Sends a POST request to passed URL with email as parameter and displays
+the body of the response
+"""
+
+import urllib.request
+import urlib.parse
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        byte = response.read()
+    print(byte.decode())
+
+
+if __name__ == '__main__':
+    main()
